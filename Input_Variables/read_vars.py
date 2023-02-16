0c75ff15fd5f88281f951591f533bbbde1710a74
Carlos Monsivais <carlosmonsivais@Carloss-MacBook-Air.local>
2023-02-15 22:44:10 -0800

    test file

--- Input_Variables/read_vars.py
+++ Input_Variables/read_vars.py
@@ -1,7 +1,7 @@
 import yaml
 import numpy as np
-import fathon
-from fathon import fathonUtils as fu
+# import fathon
+# from fathon import fathonUtils as fu
 
 with open('Input_Variables/data_vars.yaml', 'r') as file:
     input_vars=yaml.safe_load(file)
@@ -14,7 +14,7 @@ raw_data_storage=input_vars['Data_Storage']['raw_data_storage']
 ################################### Time Series Input Features ###################################
 mfdfa_win_1=input_vars['Time_Series_Input_Features']['MFDA']['win_1']
 mfdfa_win_2=input_vars['Time_Series_Input_Features']['MFDA']['win_2']
-mfdfa_wins=fu.linRangeByStep(mfdfa_win_1, mfdfa_win_2) # 30 mins to 1/2 year for
+# mfdfa_wins=fu.linRangeByStep(mfdfa_win_1, mfdfa_win_2) # 30 mins to 1/2 year for
 
 mfdfa_q_list_1=input_vars['Time_Series_Input_Features']['MFDA']['q_list_1']
 mfdfa_q_list_2=input_vars['Time_Series_Input_Features']['MFDA']['q_list_2']
