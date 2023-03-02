8daa35d4c3a03d11aed1a467326e4ae83851c624
lrjoe <ljoe@ucsd.edu>
2023-03-02 01:31:37 -0800

    include fathon in pipeline
    for running on leslie's device

--- Input_Variables/read_vars.py
+++ Input_Variables/read_vars.py
@@ -1,7 +1,7 @@
 import yaml
 import numpy as np
-# import fathon
-# from fathon import fathonUtils as fu
+import fathon
+from fathon import fathonUtils as fu
 
 with open('Input_Variables/data_vars.yaml', 'r') as file:
     input_vars=yaml.safe_load(file)
@@ -14,17 +14,14 @@ raw_data_storage=input_vars['Data_Storage']['raw_data_storage']
 ################################### Time Series Input Features ###################################
 mfdfa_win_1=input_vars['Time_Series_Input_Features']['MFDA']['win_1']
 mfdfa_win_2=input_vars['Time_Series_Input_Features']['MFDA']['win_2']
-# mfdfa_wins=fu.linRangeByStep(mfdfa_win_1, mfdfa_win_2) # 30 mins to 1/2 year for
+mfda_win_sizes=fu.linRangeByStep(mfdfa_win_1, mfdfa_win_2) # 30 mins to 1/2 year for
 
 mfdfa_q_list_1=input_vars['Time_Series_Input_Features']['MFDA']['q_list_1']
 mfdfa_q_list_2=input_vars['Time_Series_Input_Features']['MFDA']['q_list_2']
-mfdfa_q_list=np.arange(mfdfa_q_list_1, mfdfa_q_list_2)
+mfda_q_list=np.arange(mfdfa_q_list_1, mfdfa_q_list_2)
 
 mfdfa_rev_seg=input_vars['Time_Series_Input_Features']['MFDA']['rev_seg']
 mfdfa_pol_order=input_vars['Time_Series_Input_Features']['MFDA']['pol_order']
-
-fpca_min_time=input_vars['Time_Series_Input_Features']['FPCA']['min_time']
-
 
 ################################### Daily Stats Features ###################################
 daily_stats_features_lower=input_vars['Daily_Stats_Features']['lower']
