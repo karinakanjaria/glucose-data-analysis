6e0b4ec34b281bd8858b09ab75c71781b7a91a28
kbolaughlin <kolaughl@ucsd.edu>
2023-05-01 19:35:46 +0000

    add difference features creation

---
+++ Feature_Generation/difference_features.py
@@ -0,0 +1,25 @@
+import pyspark
+from pyspark.sql.functions import col, lag, when, isnull
+from pyspark.sql.types import StructType, StructField, \
+StringType, IntegerType, TimestampType, DateType, FloatType
+from pyspark.sql.window import Window
+
+
+class DifferenceFeature:
+    def add_difference_features(self, df)
+        spark = Spark_Session().spark
+        
+        my_window = Window.partitionBy('PatientId').orderBy("GlucoseDisplayTime")
+        df = df.withColumn("prev_value", lag(df.Value).over(my_window))
+        df = df.withColumn("FirstDiff", when(isnull(df.Value - df.prev_value), 0)
+                                  .otherwise(df.Value - df.prev_value))
+
+        df = df.withColumn("prev_val_sec", lag(df.FirstDiff).over(my_window))
+        df = df.withColumn("SecDiff", when(isnull(df.first_diff - df.prev_val_sec), 0)
+                                  .otherwise(df.first_diff - df.prev_val_sec))
+
+        df = df.drop('prev_value', 'prev_val_sec')
+        
+        return df
+        
+        
