df4ef5f7c1bed195d9cfab2aa8dea5ec507a3765
Karina Kanjaria <karina.kanjaria@gmail.com>
2023-05-04 06:14:16 +0000

    sleep feature adding

--- Feature_Generation/time_series_feature_creation.py
+++ Feature_Generation/time_series_feature_creation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import EntropyHub as eH
-from pyspark.sql.functions import pandas_udf, PandasUDFType
+from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
 class TS_Features:
@@ -32,8 +32,8 @@ class TS_Features:
     def poincare(self, df):
         patientid = df['PatientId'].iloc[0]
         chunk = df['Chunk'].iloc[0]
+        
         glucose_differentials = np.diff(df.Value)
-
         st_dev_differentials = np.std(np.diff(glucose_differentials))
         st_dev_values = np.std(glucose_differentials)
 
@@ -44,6 +44,50 @@ class TS_Features:
         long_term_variation = round(np.sqrt((2 * st_dev_values ** 2) - (0.5 * st_dev_differentials ** 2)), 3)
 
         ratio = round(short_term_variation / long_term_variation, 3)
+        
         poincare_df = pd.DataFrame([[patientid] + [chunk] + [short_term_variation] + [long_term_variation] + [ratio]])
         poincare_df.columns=['PatientId', 'Chunk', 'ShortTermVariance', 'LongTermVariance', 'VarianceRatio']
         return poincare_df
+    
+    
+    sleep_schema = StructType([StructField('PatientId', StringType(), True),
+                              StructField('Date', DateType(), True),
+                              StructField('SleepSDShort', FloatType(), True),
+                              StructField('SleepSDLong', FloatType(), True),
+                              StructField('SleepSDRatio', FloatType(), True)])
+    
+    @pandas_udf(sleep_schema, functionType=PandasUDFType.GROUPED_MAP)
+    def sleep(self, df):
+        patientid = df['PatientId'].iloc[0]
+        date = df['Date'].iloc[0]
+
+        sleeping = df[((df.Time >= '00:00:00') & (df.Time <= '06:00:00'))]
+        # sleep_values = np.array(sleeping.Value)
+
+        sleep_differentials = np.diff(sleeping.Value)
+        st_dev_differentials = np.std(np.diff(sleep_differentials))
+        st_dev_values = np.std(sleep_differentials)
+        
+        # measures the width of poincare cloud
+        short_term_variation = (1 / np.sqrt(2)) * st_dev_differentials
+
+        # measures the length of the poincare cloud
+        long_term_variation = np.sqrt((2 * st_dev_values ** 2) - (0.5 * st_dev_differentials ** 2))
+
+        ratio = short_term_variation / long_term_variation
+        
+        sleep_df = pd.DataFrame([[patientid] + [date] + [short_term_variation] + [long_term_variation] + [ratio]])
+        sleep_df.columns=['PatientId', 'Date', 'SleepSDShort', 'SleepSDLong', 'SleepSDRatio']
+        return sleep_df
+    
+    
+    def process_for_sleep(self, df):
+        df = df.withColumn('Date', to_date(col('GlucoseDisplayTime')))
+        df = df.withColumn('Time', date_format('GlucoseDisplayTime', 'HH:mm:ss'))
+        
+        training_df_sleep = df.groupby(['PatientId', 'Date']).apply(self.sleep)
+        return training_df_sleep
+    
+    
+    
+    
