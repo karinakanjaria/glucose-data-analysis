4f8452a697fb258ed0d1cefb3d412c9c881623b1
carlosmonsivais123 <carlos.s.monsivais@gmail.com>
2023-05-16 22:03:44 +0000

    summary stats complete

--- summary_tables_data_creation.py
+++ summary_tables_data_creation.py
@@ -7,40 +7,42 @@ from Data_Pipeline.imputation_pipeline import Date_And_Value_Imputation
 from Feature_Generation.create_binary_labels import Create_Binary_Labels
 from Feature_Generation.summary_stats import Summary_Stats_Features
 from Feature_Generation.difference_features import Difference_Features
-
 import os
 
-# Data Location
 date_and_value_imputation=Date_And_Value_Imputation()
-# Create Binary y Variables
 create_binary_labels=Create_Binary_Labels()
-# Features Daily Stats Module
 summary_stats_features=Summary_Stats_Features()
-# Features Differences
 difference_features=Difference_Features()
 
+# train, val, test
+pipeline_stage='train'
 
 training_files_directory=os.listdir('/cephfs/interpolation/train/')
-
 training_files=[i for i in training_files_directory if not ('.crc' in i or 'SUCCESS' in i)]
 
-# tester for now
-training_files=training_files[0:1]
+num_training_files=len(training_files)
+training_counter=1
 
 for training_file in training_files:
-    # Read in Data
-    training_custom_imputation_pipeline = date_and_value_imputation.read_interpolation(f'/cephfs/interpolation/train/{training_file}')
+    # Read Imputation Data
+    custom_imputation_data=date_and_value_imputation.read_interpolation(f'/cephfs/interpolation/{pipeline_stage}/{training_file}')
         
-    training_df_added_binary_labels=create_binary_labels.pyspark_binary_labels(df=training_custom_imputation_pipeline)
+    # Binary Labels
+    added_binary_labels=create_binary_labels.pyspark_binary_labels(df=custom_imputation_data)
 
-    training_df_differences = difference_features.add_difference_features(training_df_added_binary_labels)
+    # Differences Features
+    differences_df=difference_features.add_difference_features(added_binary_labels)
     
-    training_df_chunks = summary_stats_features.create_chunk_col(training_df_differences, chunk_val = 288)
+    # Create Chunk Feature
+    chunks_df=summary_stats_features.create_chunk_col(differences_df, chunk_val = 288)
     
-    training_features_summary_stats=summary_stats_features.pyspark_summary_statistics(df=training_df_chunks)
-    training_features_summary_stats.show(5)
+    # Summary Statistics Dataframe
+    features_summary_stats=summary_stats_features.pyspark_summary_statistics(df=chunks_df)
     
-    # output directory
-    # /cephfs/summary_stats/train
-    # /cephfs/summary_stats/test
-    # /cephfs/summary_stats/val
+    # Parquet File Output
+    features_summary_stats.write.format('parquet').mode("overwrite").\
+    save(f'/cephfs/summary_stats/{pipeline_stage}/summary_stats_{training_file}')
+    
+    # Counter Update
+    print(f'Completed {training_counter}/{num_training_files}')
+    training_counter=training_counter+1
