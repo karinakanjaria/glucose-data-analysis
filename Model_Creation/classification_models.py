c52d9fb6b745d4daa4a49a539669023e78007c8a
carlosmonsivais123 <carlos.s.monsivais@gmail.com>
2023-05-31 09:22:22 +0000

    while regression models running creating classification models

--- Model_Creation/classification_models.py
+++ Model_Creation/classification_models.py
@@ -1,71 +1,48 @@
-from xgboost.spark import SparkXGBRegressor
 from xgboost.spark import SparkXGBClassifier
+
 from pyspark.ml.tuning import ParamGridBuilder
 from pyspark.ml.tuning import CrossValidator
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml import Pipeline
 
-class Create_PySpark_XGBoost:        
-    def initial_training_xgboost_regression(self, ml_df, stages, random_seed, xgb_reg_model_storage_location, k=3):
-        ml_df = ml_df.withColumn("foldCol", ml_df.NumId % k)
+class Create_Classification_Models:
+    def __init__(self):
+        self.features_col="features"
+        self.label_name="target"
+        self.prediction_column_name="prediction"
+            
+    def classification_modeling(self, ml_df, stages, random_seed, classification_models_storage_locations, num_folds):
+        location_counter=0
+        model_types=[]
+        model_mapping={'XGBoost': }
         
-        features_col="features"
-        label_name="DiffPrevious"
-        
-        xgb_regression=SparkXGBRegressor(features_col=features_col, 
-                                         label_col=label_name,
-                                         random_state=random_seed,
-                                         use_gpu=True)
-        
-        # paramGrid=ParamGridBuilder().addGrid(xgb_regression.max_depth,[3, 6, 10])\
-        # .addGrid(xgb_regression.n_estimators,[10, 50, 100])\
-        # .addGrid(xgb_regression.subsample,[0.6, 0.8, 1.0])\
-        # .build()
-        paramGrid=ParamGridBuilder().addGrid(xgb_regression.max_depth,[5, 7])\
-        .addGrid(xgb_regression.n_estimators,[50, 100])\
-        .build()
-        
-        
+        ml_df=ml_df.withColumn("foldCol", ml_df.NumId % num_folds)
         
         evaluator_rmse=RegressionEvaluator(metricName='rmse',
-                                           labelCol="DiffPrevious",
-                                           predictionCol="prediction")
+                                           labelCol=self.label_name,
+                                           predictionCol=self.prediction_column_name)
+        paramGrid=ParamGridBuilder().build()
         
-        crossval=CrossValidator(estimator=xgb_regression,
-                                estimatorParamMaps=paramGrid,
-                                evaluator=evaluator_rmse,
-                                foldCol='foldCol',
-                                collectSubModels=False)
-        
-        print('Cross Validation and Hyperparameter Tuning Occuring')
-        stages.append(crossval)
-        pipeline=Pipeline(stages=stages)
-        
-        model=pipeline.fit(ml_df)
-        
-        model.write().overwrite().save(xgb_reg_model_storage_location)
-        print(f'Model Saved to {xgb_reg_model_storage_location}')
-        
-        return model
-    
-    
-    
-#     def initial_training_xgboost_classification(self, ml_df, stages, random_seed, xgb_class_model_storage_location):
-#         features_col="features"
-#         label_name="target"
-        
-#         initial_xgb_regression=SparkXGBRegressor(features_col=self.features_col, 
-#                                                  label_col=self.label_name,
-#                                                  random_state=random_seed,
-#                                                  use_gpu=False)
+        for model_type in model_types:
+            if location_counter > 0:
+                stages.pop()
+                print(f'Currently on {model_type} Model')
+            else:
+                print(f'Currently on {model_type} Model')
+            crossval=CrossValidator(estimator=model_mapping[model_type],
+                                    evaluator=evaluator_rmse,
+                                    estimatorParamMaps=paramGrid,
+                                    foldCol='foldCol',
+                                    collectSubModels=False)
 
-#         stages.append(initial_xgb_regression)
-#         pipeline=Pipeline(stages=stages)
-#         print('Model Created')
+            print('Cross Validation Occuring')
+            stages.append(crossval)
+            pipeline=Pipeline(stages=stages)
+
+            model=pipeline.fit(ml_df)
+
+            model.write().overwrite().save(regression_models_storage_locations[location_counter])
+            print(f'Model Saved to {regression_models_storage_locations[location_counter]}')
+            location_counter+=1
         
-#         model=pipeline.fit(ml_df)
-        
-#         model.write().overwrite().save(model_storage_location)
-#         print(f'Model Saved to {model_storage_location}')
-        
-#         return model
+        return None
