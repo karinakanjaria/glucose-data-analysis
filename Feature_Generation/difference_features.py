bddc86101bdc4e029549536a5c3aba8add98704d
kbolaughlin <kolaughl@ucsd.edu>
2023-06-12 20:15:18 -0700

    comments to diff features

--- Feature_Generation/difference_features.py
+++ Feature_Generation/difference_features.py
@@ -7,15 +7,20 @@ from pyspark.sql.window import Window
 
 class Difference_Features:
     def add_difference_features(self, df):
+        
         my_window = Window.partitionBy('NumId').orderBy("GlucoseDisplayTime")
+        # create column that is one lagged display time
         df = df.withColumn("prev_value", lag(df.Value).over(my_window))
+        # first diff is the difference in value between that time and previous time
         df = df.withColumn("FirstDiff", when(isnull(df.Value - df.prev_value), 0)
                                   .otherwise(df.Value - df.prev_value))
 
+        # redo above computation for second diff
         df = df.withColumn("prev_val_sec", lag(df.FirstDiff).over(my_window))
         df = df.withColumn("SecDiff", when(isnull(df.FirstDiff - df.prev_val_sec), 0)
                                   .otherwise(df.FirstDiff - df.prev_val_sec))
 
+        #drop unnecessary columns
         df = df.drop('prev_value', 'prev_val_sec')
         
         return df
